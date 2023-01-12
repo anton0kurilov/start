8862d846493220b89d421caee81842c28447dfa6
Anton Kurilov <anton0kurilov@gmail.com>
2023-01-12 10:35:02 +0700

    refactor: remove unused data

--- src/scripts/parser.js
+++ src/scripts/parser.js
@@ -4,27 +4,22 @@ const moment = require('moment'),
 let data = {
     TheVerge: {
         rss: 'https://www.theverge.com/rss/index.xml',
-        element: '#col1',
         icon: 'https://www.theverge.com/favicon.ico',
     },
     MacRumors: {
         rss: 'https://feeds.macrumors.com/MacRumors-All',
-        element: '#col2',
         icon: 'https://macrumors.com/favicon.ico',
     },
     NinetoFiveGoogle: {
         rss: 'https://9to5google.com/feed/',
-        element: '#col3',
         icon: 'https://9to5google.com/favicon.ico',
     },
     Vedomosti: {
         rss: 'https://www.vedomosti.ru/rss/rubric/technology.xml',
-        element: '#col4',
         icon: 'https://www.vedomosti.ru/favicon.ico',
     },
     TechMeme: {
         rss: 'https://www.techmeme.com/feed.xml',
-        element: '#col5',
         icon: 'https://www.techmeme.com/favicon.ico',
     },
 }
