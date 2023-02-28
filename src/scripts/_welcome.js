d4b20542b1996c1f97d84c2d0e202c4702dbdd64
Anton Kurilov <anton0kurilov@gmail.com>
2023-02-28 11:18:11 +0700

    feat: show current time in Welcome screen

--- src/scripts/_welcome.js
+++ src/scripts/_welcome.js
@@ -1,8 +1,28 @@
 // create a Welcome block and push it to the page
-let welcomeBody = document.createElement('div'),
-    welcomeHeader = '<h1 class="welcome__header">Hello, Friend</h1>'
+const welcomeBody = document.createElement('div'),
+    monthsArr = [
+        'января',
+        'февраля',
+        'марта',
+        'апреля',
+        'мая',
+        'июня',
+        'июля',
+        'августа',
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря',
+    ],
+    dateObj = new Date(),
+    dateMinutes = String(dateObj.getMinutes()).padStart(2, '0'),
+    dateMonth = dateObj.getMonth(),
+    welcomeHeader = '<h1 class="welcome__header">Hello, Friend</h1>',
+    welcomeTime = `<h3 class="welcome__time">Сейчас ${dateObj.getHours()}:${dateMinutes}, ${dateObj.getDate()} ${
+        monthsArr[dateMonth]
+    }</h3>`
 welcomeBody.className = 'welcome'
-welcomeBody.innerHTML = welcomeHeader
+welcomeBody.innerHTML = welcomeHeader + welcomeTime
 document.querySelector('body').appendChild(welcomeBody)
 
 // hide the Welcome block after 3s
