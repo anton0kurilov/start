1c5434517e02ae6bdb0981be1c2c9162b7fd7a31
Anton Kurilov <anton0kurilov@gmail.com>
2023-02-28 10:45:23 +0700

    refactor: use Promise to wait for all columns to load

--- src/scripts/_events.js
+++ src/scripts/_events.js
@@ -1,5 +1,14 @@
 // Make columns header clickable for scrolling to the top of feeds
-window.setTimeout(() => {
+// Thanks to https://stackoverflow.com/questions/16149431/make-function-wait-until-element-exists/53269990#53269990
+
+const isElementLoaded = async (selector) => {
+    while (document.querySelector(selector) === null) {
+        await new Promise((resolve) => requestAnimationFrame(resolve))
+    }
+    return document.querySelector(selector)
+}
+
+isElementLoaded('#col7').then((selector) => {
     const columnHeaderElement = document.querySelectorAll(
         '.body__column-header-container'
     )
@@ -8,4 +17,4 @@ window.setTimeout(() => {
             this.parentElement.scrollTo({top: 0, behavior: 'smooth'})
         })
     }
-}, 3000)
+})
