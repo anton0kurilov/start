08bf1a91d3c057ac0c18c51645fab3b8abf6135e
Anton Kurilov <anton0kurilov@gmail.com>
2023-02-28 14:59:51 +0700

    fix: change time language

--- src/scripts/_welcome.js
+++ src/scripts/_welcome.js
@@ -1,26 +1,26 @@
 // create a Welcome block and push it to the page
 const welcomeBody = document.createElement('div'),
     monthsArr = [
-        'января',
-        'февраля',
-        'марта',
-        'апреля',
-        'мая',
-        'июня',
-        'июля',
-        'августа',
-        'сентября',
-        'октября',
-        'ноября',
-        'декабря',
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
     ],
     dateObj = new Date(),
     dateMinutes = String(dateObj.getMinutes()).padStart(2, '0'),
     dateMonth = dateObj.getMonth(),
     welcomeHeader = '<h1 class="welcome__header">Hello, Friend</h1>',
-    welcomeTime = `<h3 class="welcome__time">Сейчас ${dateObj.getHours()}:${dateMinutes}, ${dateObj.getDate()} ${
+    welcomeTime = `<h3 class="welcome__time">${dateObj.getHours()}:${dateMinutes} • ${
         monthsArr[dateMonth]
-    }</h3>`
+    }, ${dateObj.getDate()}</h3>`
 welcomeBody.className = 'welcome'
 welcomeBody.innerHTML = welcomeHeader + welcomeTime
 document.querySelector('body').appendChild(welcomeBody)
