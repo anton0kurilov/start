6c38ae1c947254d498f6fb7ea3891a2e8bbc6f56
Anton Kurilov <anton0kurilov@gmail.com>
2023-05-29 10:31:36 +0700

    refactor: remove an unused variable

--- src/scripts/_admin.js
+++ src/scripts/_admin.js
@@ -1,20 +1,6 @@
 const bodyElement = document.querySelector('body')
 const moment = require('moment'),
     adminElement = document.createElement('div'),
-    monthsArr = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-    ],
     dateObj = new Date(),
     dateMinutes = String(dateObj.getMinutes()).padStart(2, '0'),
     dateHours = String(dateObj.getHours()).padStart(2, '0')
