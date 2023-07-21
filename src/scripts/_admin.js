b74c881d300d72ad34172b495423cfaab1e367bc
Anton Kurilov <anton0kurilov@gmail.com>
2023-07-21 20:29:38 +0700

    feat: add refresh page button

--- src/scripts/_admin.js
+++ src/scripts/_admin.js
@@ -3,7 +3,15 @@ const moment = require('moment'),
     adminElement = document.createElement('div'),
     dateObj = new Date(),
     dateMinutes = String(dateObj.getMinutes()).padStart(2, '0'),
-    dateHours = String(dateObj.getHours()).padStart(2, '0')
+    dateHours = String(dateObj.getHours()).padStart(2, '0'),
+    refreshButtonContent =
+        '<a id="refreshButton" title="Refresh this" class="admin__refresh">↻</a>'
 adminElement.className = 'admin'
-adminElement.innerHTML = `Last updated at <b>${dateHours}:${dateMinutes}</b>`
+adminElement.innerHTML = `Last updated at <b>${dateHours}:${dateMinutes}</b> ${refreshButtonContent}`
 bodyElement.appendChild(adminElement)
+
+// Update the page
+const refreshButtonElement = document.querySelector('#refreshButton')
+refreshButtonElement.addEventListener('click', () => {
+    location.reload()
+})
