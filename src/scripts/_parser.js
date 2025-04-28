d4141e4bbf5bc94633d5a612b41c652e125abbba
Anton Kurilov <anton0kurilov@gmail.com>
2025-04-28 21:49:18 +0700

    feat: change error showing behavior

--- src/scripts/_parser.js
+++ src/scripts/_parser.js
@@ -93,7 +93,8 @@ async function loadFeeds() {
         const existingError = document.querySelector('.error')
         if (existingError) existingError.remove()
 
-        // create an error badge and push it to the page
+        // Create an error badge and push it to the page
+        console.log(err.message)
         let errorElement = document.createElement('div'),
             errorCloseElement =
                 '<div id="errClose" class="error-close"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#e3e3e3"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"/></svg></div>'
@@ -108,6 +109,9 @@ async function loadFeeds() {
                 errorElement.remove()
                 metaColorChanger()
             })
+        setInterval(function () {
+            document.querySelector('.error').remove()
+        }, 15000)
     }
 
     // Only return hideWelcome on initial load
